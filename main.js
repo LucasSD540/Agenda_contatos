d9dcf5530563fb4bedef53216ddc291441b394f1
Lucas Souza <lucas.souzaduarte.73@gmail.com>
2023-06-30 16:18:12 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form-contact');
+const nome = [];
+const numero = [];
+
+let linhas = ''; 
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('contact-name');
+    const inputNumeroContato = document.getElementById('contact-number');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`
+    linha += `<td>${inputNumeroContato.value}</td>`
+
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+}
